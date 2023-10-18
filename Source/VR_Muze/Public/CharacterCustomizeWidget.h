4f40327b8eb629ec3db4b93f593e366fba8e3940
Walkerworker <fghj160@naver.com>
2023-10-18 17:21:29 +0900

    캐릭터 커스터마이즈 위젯 C++만 추가

---
+++ Source/VR_Muze/Public/CharacterCustomizeWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterCustomizeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_MUZE_API UCharacterCustomizeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
