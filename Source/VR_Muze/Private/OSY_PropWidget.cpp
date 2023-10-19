39313866dc908a984f8291a8e073321e764b7c06
Walkerworker <fghj160@naver.com>
2023-10-19 15:00:45 +0900

    1019_1

--- Source/VR_Muze/Private/OSY_PropWidget.cpp
+++ Source/VR_Muze/Private/OSY_PropWidget.cpp
@@ -2,4 +2,31 @@
 
 
 #include "OSY_PropWidget.h"
+#include "Components/Button.h"
+#include "KJS_TestSpawnActor.h"
 
+void UOSY_PropWidget::NativeConstruct()
+{
+	btn_BoxSpawn->OnClicked.AddDynamic(this, &UOSY_PropWidget::SpawnBox);
+
+
+}
+
+void UOSY_PropWidget::SpawnBox()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		//버튼 클릭하면
+		//스폰
+		//어디에?
+		//맵의 지정된 위치에
+
+		FActorSpawnParameters param;
+		FVector spawnLoc = FVector(0,0,0);
+		FRotator spawnRot = FRotator(0,0,0);
+		param.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		//AKJS_TestSpawnActor* Cube = World->SpawnActor<AKJS_TestSpawnActor>(AKJS_TestSpawnActor::StaticClass(), spawnLoc, spawnRot, param);
+		
+	}
+}
--- Source/VR_Muze/Public/OSY_PropWidget.h
+++ Source/VR_Muze/Public/OSY_PropWidget.h
@@ -14,4 +14,20 @@ class VR_MUZE_API UOSY_PropWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget), Category=Button)
+	class UButton* btn_BoxSpawn;
+
+	//UPROPERTY(VisibleAnywhere)
+	//class AKJS_TestSpawnActor* Cube;
+
+
+public:
+	UFUNCTION()
+	void SpawnBox();
+
+	
 };
